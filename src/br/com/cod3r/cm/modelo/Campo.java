63fbfbc6decc6dd11821a7faa9d39c865ec1b7f2
Arthur Detomi <arthurdetomi@gmail.com>
2023-02-10 18:46:38 -0300

    Classe Campo
    Criando classe que representa cada campo do jogo

---
+++ src/br/com/cod3r/cm/modelo/Campo.java
@@ -0,0 +1,20 @@
+package br.com.cod3r.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campo {
+
+    private final int linha;
+    private final int coluna;
+    private boolean aberto = false;
+    private boolean minado = false;
+    private boolean marcado = false;
+    private List<Campo> vizinhos = new ArrayList<>();
+
+    Campo(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+}
